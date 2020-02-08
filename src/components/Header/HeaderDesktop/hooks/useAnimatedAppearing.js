7546977f6536da43a5b8cc2ff18f06c8cb4fefef
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2020-02-08 13:48:02 +0200

    ESLint (#5)
    ESLint configuration added. Code updated to match eslint rules. Readme updated

--- src/components/Header/HeaderDesktop/hooks/useAnimatedAppearing.js
+++ src/components/Header/HeaderDesktop/hooks/useAnimatedAppearing.js
@@ -1,15 +1,17 @@
 import {useSprings} from 'react-spring'
 
 export function useAnimatedAppearing() {
-  const appearingSprings = useSprings(3, [...Array(3)].map((i, n) => ({
+  const appearingSprings = useSprings(4, [...Array(4)].map((i, n) => ({
     config: {mass: 1, tension: 280, friction: 50},
     from: {
       opacity: 0,
-      transform: `translate3d(-20px, 0, 0)`
+      transform: 'translate3d(-20px, 0, 0)',
+      reverseTransform: 'translate3d(5px, 0, 0)'
     },
     to: {
       opacity: 1,
-      transform: 'translate3d(0, 0, 0)'
+      transform: 'translate3d(0, 0, 0)',
+      reverseTransform: 'translate3d(0, 0, 0)'
     },
     delay: n === 0 ? 0 : 180 * n
   })))
