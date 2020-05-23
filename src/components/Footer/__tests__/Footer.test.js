78356eb2a1b7e1cc952d33a140d7b084e6fcec16
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2020-05-23 23:25:45 +0300

    Footer.test refactor

--- src/components/Footer/__tests__/Footer.test.js
+++ src/components/Footer/__tests__/Footer.test.js
@@ -1,15 +1,46 @@
 import React from 'react'
+import {MemoryRouter} from 'react-router-dom'
 import {render} from '@testing-library/react'
+import {renderHook} from '@testing-library/react-hooks'
+import user from '@testing-library/user-event'
 
+import {ThemeProvider, useTheme} from 'contexts'
+import {PRIME_ROUTES} from 'constants/index'
 import Footer from '..'
 
-// TODO remove mocks and do integrational tests
-jest.mock('components/DarkThemeSwitcher', () => () => 'MockDarkThemeSwitcher')
-jest.mock('components/LangSelector', () => () => 'MockLangSelector')
-jest.mock('components//SocialMediaIcons', () => () => 'MockSocialMediaIcons')
-jest.mock('components/Footer/FooterNavigation', () => () => 'MockFooterNavigation')
+const setup = () => render(<Footer />, {
+  wrapper: props => (
+    <MemoryRouter>
+      <ThemeProvider {...props} />
+    </MemoryRouter>
+  )
+})
 
-test('should match snapshot', () => {
-  const {asFragment} = render(<Footer />)
-  expect(asFragment()).toMatchSnapshot()
+test('renders and acts as expected', () => {
+  const {getByRole} = setup()
+
+  // navigation
+  const expectedNavLinks = Object.values(PRIME_ROUTES).map(obj => obj.name)
+
+  const actualNavLinks = Array.from(
+    getByRole('navigation').querySelectorAll('li')
+  ).map(li => li.textContent)
+
+  expect(expectedNavLinks).toEqual(actualNavLinks)
+
+  // theme switching
+  const getThemeHook = () => renderHook(() => useTheme(), {
+    wrapper: ThemeProvider
+  })
+  const themeSwitcher = getByRole('checkbox')
+
+  expect(getThemeHook().result.current.isDarkTheme).toBeFalsy()
+
+  user.click(themeSwitcher)
+  expect(getThemeHook().result.current.isDarkTheme).toBeTruthy()
+
+  user.click(themeSwitcher)
+  expect(getThemeHook().result.current.isDarkTheme).toBeFalsy()
+
+  // TODO: switching language
 })
