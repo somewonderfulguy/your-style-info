5d217612cc4fec9daab113541aa4363374d74fe8
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2020-05-23 18:51:26 +0300

    refactor HamburgerIcon.test

--- src/components/Header/HeaderMobile/HamburgerIcon/__tests__/HamburgerIcon.test.js
+++ src/components/Header/HeaderMobile/HamburgerIcon/__tests__/HamburgerIcon.test.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import {render} from '@testing-library/react'
+import {act, renderHook} from '@testing-library/react-hooks'
 
-import * as mockThemeContext from 'contexts/themeContext'
+import {ThemeProvider, useTheme} from 'contexts'
 import HamburgerIcon from '..'
 
-jest.spyOn(mockThemeContext, 'useTheme').mockImplementation(() => ({isDarkTheme: false}))
-afterEach(() => jest.clearAllMocks())
+const setup = () => render(<HamburgerIcon />, {wrapper: ThemeProvider})
 
+// TODO remove all awaits once react-spring 9.0.0 released
 test('snapshot diffrenece between default and clicked states', async () => {
-  const {asFragment, rerender} = render(<HamburgerIcon />)
+  const {asFragment, rerender} = setup()
   const defaultState = asFragment()
+
   rerender(<HamburgerIcon isOpen />)
-
-  // TODO remove once react-spring 9.0.0 released
   await new Promise((r) => setTimeout(r, 400))
-
   const clickedState = asFragment()
 
   expect(defaultState).toMatchDiffSnapshot(clickedState, {
@@ -22,16 +21,20 @@ test('snapshot diffrenece between default and clicked states', async () => {
     aAnnotation: 'default state (three lines)',
     bAnnotation: 'clicked state (cross)'
   })
-  expect(mockThemeContext.useTheme).toHaveBeenCalledTimes(2)
 
-  // TODO close and compare
+  rerender(<HamburgerIcon isOpen={false} />)
+  await new Promise((r) => setTimeout(r, 400))
+  const unclickedState = asFragment()
+
+  expect(unclickedState).toEqual(defaultState)
 })
 
 test('snapshot difference in dark/light theme', async () => {
-  const lightTheme = render(<HamburgerIcon />).asFragment()
+  const {result} = renderHook(() => useTheme(), {wrapper: ThemeProvider})
 
-  mockThemeContext.useTheme.mockReturnValueOnce({isDarkTheme: true})
-  const darkTheme = render(<HamburgerIcon />).asFragment()
+  const lightTheme = setup().asFragment()
+  act(() => result.current.switchTheme())
+  const darkTheme = setup().asFragment()
 
   expect(lightTheme).toMatchDiffSnapshot(darkTheme, {
     contextLines: 1,
@@ -39,7 +42,8 @@ test('snapshot difference in dark/light theme', async () => {
     bAnnotation: 'dark theme'
   })
 
-  expect(mockThemeContext.useTheme).toHaveBeenCalledTimes(2)
-
-  // TODO switch back and compare
+  // back to light check
+  act(() => result.current.switchTheme())
+  const backToLightTheme = setup().asFragment()
+  expect(backToLightTheme).toEqual(lightTheme)
 })
