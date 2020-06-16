6a9557c3237388fd84ab134b3eb5fcb2fbab4914
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2020-06-16 20:38:47 +0300

    fix failing tests, improve tests

--- src/components/LangSelector/__tests__/LangSelector.test.js
+++ src/components/LangSelector/__tests__/LangSelector.test.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import {act} from '@testing-library/react'
+import {act, screen} from '@testing-library/react'
 import user from '@testing-library/user-event'
 
 import {renderWithRouter} from 'shared'
-import * as mockThemeContext from 'contexts/themeContext'
+import * as spyThemeContext from 'contexts/themeContext'
+import {LocalisationProvider} from 'contexts/localisationContext'
 import LangSelector from '..'
 
-jest.spyOn(mockThemeContext, 'useTheme').mockImplementation(() => ({isDarkTheme: false}))
+jest.spyOn(spyThemeContext, 'useTheme').mockImplementation(() => ({isDarkTheme: false}))
+jest.spyOn(window, 'fetch')
+
+jest.mock('components/Image', () => () => 'mock-image')
+
 afterEach(() => jest.clearAllMocks())
 
 expect.addSnapshotSerializer({
@@ -14,21 +19,29 @@ expect.addSnapshotSerializer({
   print(val) {return val.replace(/\n\+\s{0,}<ul(\n.*){1,}/gm, '')}
 })
 
-const setup = (showAbove, gray, path = '/') => (
-  renderWithRouter(
+const setup = (showAbove, gray, path = '/') => {
+  const utils = renderWithRouter(
     <LangSelector showAbove={showAbove} gray={gray} />,
+    {wrapper: LocalisationProvider},
     {route: path}
   )
-)
+
+  const getSwitchLanguageBtn = () => screen.getByLabelText(/switch language/i)
+
+  return {
+    ...utils,
+    getSwitchLanguageBtn
+  }
+}
 
 test('snapshot diff: dropdown above/below, default/gray colour, light/dark mode', () => {
-  mockThemeContext.useTheme.mockReturnValueOnce({isDarkTheme: false})
+  spyThemeContext.useTheme.mockReturnValueOnce({isDarkTheme: false})
   const componentDefault = setup().asFragment()
 
-  mockThemeContext.useTheme.mockReturnValueOnce({isDarkTheme: true})
+  spyThemeContext.useTheme.mockReturnValueOnce({isDarkTheme: true})
   const componentWithProps = setup(true, true).asFragment()
 
-  expect(mockThemeContext.useTheme).toHaveBeenCalledTimes(2)
+  expect(spyThemeContext.useTheme).toHaveBeenCalledTimes(2)
   expect(componentDefault).toMatchDiffSnapshot(componentWithProps, {
     contextLines: 1,
     aAnnotation: 'default props and theme',
@@ -43,9 +56,9 @@ test('snapshot diff: aria state and css-class of triangle when open/hidden menu 
     bAnnotation: 'expanded menu'
   }
 
-  const {getByLabelText, asFragment, rerender} = setup()
+  const {getSwitchLanguageBtn, asFragment, rerender} = setup()
   const beforeClick = asFragment()
-  const switchLanguageBtn = getByLabelText('Switch language')
+  const switchLanguageBtn = getSwitchLanguageBtn()
 
   user.click(switchLanguageBtn)
   expect(beforeClick).toMatchDiffSnapshot(asFragment(), diffSnapshotOptions)
@@ -58,40 +71,41 @@ test('snapshot diff: aria state and css-class of triangle when open/hidden menu 
   expect(beforeClickShowAbove).toMatchDiffSnapshot(asFragment(), diffSnapshotOptions)
 })
 
-test('should show/hide menu when clicking on language selector', () => {
-  const url = '/en/outerwear/trench-coat'
-  const {getByLabelText, getByRole, queryByRole, history} = setup(false, false, url)
-  const langSelectorBtn = getByLabelText(/switch language/i)
+test('should show/hide menu when clicking on language selector', async () => {
+  const promise = Promise.resolve({
+    ok: true,
+    json: async () => ({header: 'mockData', components: []})
+  })
+  window.fetch.mockReturnValueOnce(promise)
 
-  expect(queryByRole('menu')).not.toBeInTheDocument()
+  const url = '/en/outerwear/trench-coat'
+  const {getSwitchLanguageBtn} = setup(false, false, url)
+  const langSelectorBtn = getSwitchLanguageBtn()
+
+  expect(screen.queryByRole('menu')).not.toBeInTheDocument()
 
   // open
   user.click(langSelectorBtn)
-  expect(getByRole('menu')).toBeInTheDocument()
+  expect(screen.getByRole('menu')).toBeInTheDocument()
 
-  // select language - changes language & closes menu
-  const beforeChange = url
-  const afterChange = url.replace(/^\/en/, '/ru')
-
-  expect(history.location.pathname).toEqual(beforeChange)
-
-  user.click(getByRole('menu').querySelector('button:not(:disabled)'))
-
-  expect(queryByRole('menu')).not.toBeInTheDocument()
-  expect(history.location.pathname).toEqual(afterChange)
+  // select language - makes api call, hides menu
+  user.click(screen.getByRole('menu').querySelector('button:not(:disabled)'))
+  await act(() => promise)
+  expect(screen.queryByRole('menu')).not.toBeInTheDocument()
+  expect(window.fetch).toHaveBeenCalledTimes(1)
 
   // open
   user.click(langSelectorBtn)
 
   // close
   user.click(langSelectorBtn)
-  expect(queryByRole('menu')).not.toBeInTheDocument()
+  expect(screen.queryByRole('menu')).not.toBeInTheDocument()
 
   // open
   user.click(langSelectorBtn)
 
   // close by clicking outside
-  user.click(getByLabelText(/switch language/i))
+  user.click(screen.getByLabelText(/switch language/i))
   act(() => user.click(document.body))
-  expect(queryByRole('menu')).not.toBeInTheDocument()
+  expect(screen.queryByRole('menu')).not.toBeInTheDocument()
 })
