96de57c0325203c553c383371ee85e2ce4c14737
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2021-06-29 23:29:53 +0300

    fix couple tests

--- src/contexts/__tests__/screenDimensionsContext.test.js
+++ src/contexts/__tests__/screenDimensionsContext.test.js
@@ -5,7 +5,7 @@ import {ScreenDimensionsProvider, useScreenDimensions} from '..'
 test('useScreenDimensions should throw error if used outside ScreenDimensionsProvider', () => {
   // throw error
   const {result: {error}} = renderHook(() => useScreenDimensions())
-  expect(error).toEqual(new Error('useLocalization must be used within a LocalizationProvider'))
+  expect(error).toEqual(new Error('useScreenDimensions must be used within a ScreenDimensionsProvider'))
 
   // work properly
   expect(() => {
--- src/shared/tests.js
+++ src/shared/tests.js
@@ -3,21 +3,30 @@ import {render as rtlRender} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {Router} from 'react-router-dom'
 import {createMemoryHistory} from 'history'
+import {QueryClient, QueryClientProvider} from 'react-query'
 
-import {HeaderHeightProvider, LocalisationProvider, ScreenDimensionsProvider, ThemeProvider} from 'contexts'
+import {HeaderHeightProvider, LocalizationProvider, ScreenDimensionsProvider, ThemeProvider} from 'contexts'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    initialStale: true
+  }
+})
 
 const render = (ui, {route = '/', ...options} = {}) => {
   const history = createMemoryHistory({initialEntries: [route]})
   const Wrapper = props => (
-    <Router history={history}>
-      <HeaderHeightProvider>
-        <LocalisationProvider>
-          <ThemeProvider>
-            <ScreenDimensionsProvider {...props} />
-          </ThemeProvider>
-        </LocalisationProvider>
-      </HeaderHeightProvider>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router history={history}>
+        <HeaderHeightProvider>
+          <LocalizationProvider>
+            <ThemeProvider>
+              <ScreenDimensionsProvider {...props} />
+            </ThemeProvider>
+          </LocalizationProvider>
+        </HeaderHeightProvider>
+      </Router>
+    </QueryClientProvider>
   )
   return {
     ...rtlRender(ui, {wrapper: Wrapper, ...options}),
