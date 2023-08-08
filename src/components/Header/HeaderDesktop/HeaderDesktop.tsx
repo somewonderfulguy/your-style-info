0e33ba52c83a465f1312bffb07532eedd9c9659e
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2023-08-09 00:10:38 +0300

    test commit

--- src/components/Header/HeaderDesktop/HeaderDesktop.tsx
+++ src/components/Header/HeaderDesktop/HeaderDesktop.tsx
@@ -1,18 +1,25 @@
-import React, {useEffect, useRef, useState} from 'react'
-import {animated, useSpring, useTransition} from 'react-spring'
+import React, { useEffect, useRef, useState } from 'react'
+import { animated, useSpring, useTransition } from 'react-spring'
 
-import {useAnimatedAppearing} from './hooks'
-import {useStickyNavBar} from '../hooks'
+import { useAnimatedAppearing } from './hooks'
+import { useStickyNavBar } from '../hooks'
 import HeadNavigationDesktop from './HeadNavigationDesktop'
 import DarkThemeSwitcher from 'components/DarkThemeSwitcher'
 import LangSelector from 'components/LangSelector'
 import SocialMediaIcons from 'components/SocialMediaIcons'
-import {useHeaderHeightDispatch, useLocalization} from 'contexts'
+import { useHeaderHeightDispatch, useLocalization } from 'contexts'
 import styles from './HeaderDesktop.module.css'
 
 const HeaderDesktop = () => {
-  const [, , {data: translations}] = useLocalization()
-  const {appearingSprings: [firstAppearing, secondAppearing, thirdAppearing, forthAppearing]} = useAnimatedAppearing()
+  const [, , { data: translations }] = useLocalization()
+  const {
+    appearingSprings: [
+      firstAppearing,
+      secondAppearing,
+      thirdAppearing,
+      forthAppearing
+    ]
+  } = useAnimatedAppearing()
 
   const setHeaderHeight = useHeaderHeightDispatch()
 
@@ -35,27 +42,30 @@ const HeaderDesktop = () => {
   // show on scroll-up logic
   const [isRootMenuOpen, setRootMenuOpen] = useState(false)
   const [persistRootMenu, setPersistRootMenu] = useState(false)
-  const navBarState = useStickyNavBar(navBarTopLine + (persistRootMenu ? 0 : navbarHeight), navBarTopLine)
-  const {isFixed, isShown} = navBarState
+  const navBarState = useStickyNavBar(
+    navBarTopLine + (persistRootMenu ? 0 : navbarHeight),
+    navBarTopLine
+  )
+  const { isFixed, isShown } = navBarState
   useEffect(() => void setRootMenuOpen(false), [navBarState])
 
   // animate menu on scroll up
-  const {top: menuTop} = useSpring({
-    config: {duration: 200},
+  const { top: menuTop } = useSpring({
+    config: { duration: 200 },
     top: isShown || isRootMenuOpen || persistRootMenu ? -1 : navbarHeight * -1
   })
 
   const subtitleSelector = 'subtitle'
   const transitions = useTransition(translations?.subtitle, null, {
-    from: {opacity: 0},
-    enter: {opacity: 1},
-    leave: {opacity: 0}
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
   })
 
   return (
     <header
       className={styles.header}
-      style={{paddingBottom: navbarHeight}}
+      style={{ paddingBottom: navbarHeight }}
       ref={headerDOM}
     >
       <div className={styles.aboveNav}>
@@ -63,34 +73,50 @@ const HeaderDesktop = () => {
           <animated.h1 className={styles.title} style={firstAppearing}>
             Your Style
           </animated.h1>
-          <animated.p className={styles.subtitleContainer} style={secondAppearing}>
-            {transitions.map(({item, key, props}) => (
-              item !== subtitleSelector && (
-                <animated.span key={key} style={props} className={styles.subtitle}>
-                  {item}
-                </animated.span>
-              )
-            ))}
+          <animated.p
+            className={styles.subtitleContainer}
+            style={secondAppearing}
+          >
+            {transitions.map(
+              ({ item, key, props }) =>
+                item !== subtitleSelector && (
+                  <animated.span
+                    key={key}
+                    style={props}
+                    className={styles.subtitle}
+                  >
+                    {item}
+                  </animated.span>
+                )
+            )}
           </animated.p>
         </div>
         <div className={styles.sideControlsContainer}>
-          <animated.div style={{
-            opacity: secondAppearing.opacity,
-            transform: secondAppearing.reverseTransform,
-            zIndex: 1
-          }}>
+          <animated.div
+            style={{
+              opacity: secondAppearing.opacity,
+              transform: secondAppearing.reverseTransform,
+              zIndex: 1
+            }}
+          >
             <LangSelector />
           </animated.div>
-          <animated.div className={styles.sideControlsSocialMedia} style={{
-            opacity: thirdAppearing.opacity,
-            transform: thirdAppearing.reverseTransform
-          }}>
+          <animated.div
+            className={styles.sideControlsSocialMedia}
+            style={{
+              opacity: thirdAppearing.opacity,
+              transform: thirdAppearing.reverseTransform
+            }}
+          >
             <SocialMediaIcons small color="lightgray" />
           </animated.div>
-          <animated.div className={styles.sideControlsDarkTheme} style={{
-            opacity: forthAppearing.opacity,
-            transform: forthAppearing.reverseTransform
-          }}>
+          <animated.div
+            className={styles.sideControlsDarkTheme}
+            style={{
+              opacity: forthAppearing.opacity,
+              transform: forthAppearing.reverseTransform
+            }}
+          >
             <DarkThemeSwitcher />
           </animated.div>
         </div>
@@ -106,7 +132,10 @@ const HeaderDesktop = () => {
         onMouseEnter={() => setPersistRootMenu(true)}
         onMouseLeave={() => setPersistRootMenu(false)}
       >
-        <HeadNavigationDesktop setRootMenuOpen={setRootMenuOpen} setPersistRootMenu={setPersistRootMenu} />
+        <HeadNavigationDesktop
+          setRootMenuOpen={setRootMenuOpen}
+          setPersistRootMenu={setPersistRootMenu}
+        />
       </animated.nav>
     </header>
   )
