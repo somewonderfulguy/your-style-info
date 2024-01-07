b252a6d4244e94925cb448001dd795b61e7b9fa1
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2024-01-07 23:28:11 +0200

    carousel in progress - implemented basic view, fix mobile

--- src/components/TilesCarousel/TilesCarousel.tsx
+++ src/components/TilesCarousel/TilesCarousel.tsx
@@ -1,7 +1,6 @@
 import useEmblaCarousel from 'embla-carousel-react'
 
 import { TilesCarouselComponent } from '~api/pageApi'
-import classNames from '~shared/utils/classNames'
 import useResizeObserver from '~shared/hooks/useResizeObserver'
 
 import pageStyles from '~components/Page/Page.module.css'
@@ -10,18 +9,19 @@ import styles from './TilesCarousel.module.css'
 const TilesCarousel = ({
   contentWidth = 'content'
 }: TilesCarouselComponent) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [bindResizeObserver, { width }] = useResizeObserver<HTMLDivElement>()
   const [emblaRef] = useEmblaCarousel()
 
   return (
-    <div
-      ref={bindResizeObserver}
-      className={classNames(pageStyles[contentWidth], styles.wrapper)}
-    >
-      <div ref={emblaRef}>
-        <div className={styles.container}>
-          <div className={styles.card}>
+    <div ref={bindResizeObserver} className={pageStyles[contentWidth]}>
+      <div ref={emblaRef} style={{ width: width }} className={styles.wrapper}>
+        <div className={styles.cardContainer}>
+          <div
+            style={{
+              backgroundImage: 'url(/img/thumbs/trench_thumb.jpg)'
+            }}
+            className={styles.card}
+          >
             <img src="/img/thumbs/trench_thumb.jpg" alt="" />
             <div className={styles.cardInfo}>
               <h3>Trench coat</h3>
@@ -30,8 +30,21 @@ const TilesCarousel = ({
                 description, history.
               </p>
             </div>
+            <div className={styles.cardAdditionalInfo}>
+              <div>
+                <div>👁 23 🗨 3</div>
+              </div>
+              <div>
+                <div>07 Jan 2024</div>
+              </div>
+            </div>
           </div>
-          <div className={styles.card}>
+          <div
+            style={{
+              backgroundImage: 'url(/img/thumbs/baudoin_lange_thumb.jpg)'
+            }}
+            className={styles.card}
+          >
             <img src="/img/thumbs/baudoin_lange_thumb.jpg" alt="" />
             <div className={styles.cardInfo}>
               <h3>Suede shoes care</h3>
@@ -40,12 +53,35 @@ const TilesCarousel = ({
                 condition.
               </p>
             </div>
+            <div className={styles.cardAdditionalInfo}>
+              <div>
+                <div>👁 23</div>
+                <div>🗨 3</div>
+              </div>
+              <div className={styles.dates}>
+                <div>11 Nov 2023</div>
+                <div>last upd. 07 Jan 2024</div>
+              </div>
+            </div>
           </div>
-          <div className={styles.card}>
+          <div
+            style={{
+              backgroundImage: 'url(/img/thumbs/white_sneakers_thumb.jpg)'
+            }}
+            className={styles.card}
+          >
             <img src="/img/thumbs/white_sneakers_thumb.jpg" alt="" />
             <div className={styles.cardInfo}>
               <h3>White leather sneakers cleaning</h3>
               <p>Guide on how to clean your white leather sneakers.</p>
+            </div>
+            <div className={styles.cardAdditionalInfo}>
+              <div>
+                <div>👁 23 🗨 3</div>
+              </div>
+              <div>
+                <div>07 Jan 2024</div>
+              </div>
             </div>
           </div>
         </div>
